4f657b9d0f789ad72317cceda5389b6f4e1ffa7a
dj8552 <cm684318@gmail.com>
2024-05-02 18:38:38 -0600

    start

---
+++ ClientPlugin/Plugin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Sandbox.Game.Entities.Blocks;
+using Sandbox.Game.GameSystems.BankingAndCurrency;
+using Sandbox.Game.Screens.ViewModels;
+using Sandbox.Game.World;
+using Sandbox.ModAPI;
+using VRage.Plugins;
+
+namespace ClientPlugin
+{
+    // ReSharper disable once UnusedType.Global
+    public class Plugin : IPlugin, IDisposable
+    {
+        public const string Name = "StoreFix";
+        public static Plugin Instance { get; private set; }
+
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+        public void Init(object gameInstance)
+        {
+            Instance = this;
+
+            Harmony harmony = new Harmony(Name);
+
+            var StoreGUIBuy = AccessTools.Method(typeof(MyStoreBlockViewModel), "UpdateTotalPriceToBuy");
+            harmony.Patch(StoreGUIBuy, null, new HarmonyMethod(typeof(Plugin), "UpdateTotalBuyPricePatch"));
+
+            var StoreGUISell = AccessTools.Method(typeof(MyStoreBlockViewModel), "UpdateTotalPriceToSell");
+            harmony.Patch(StoreGUISell, null, new HarmonyMethod(typeof(Plugin), "UpdateTotalSellPricePatch"));
+        }
+
+        public void Dispose()
+        {
+            Instance = null;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        private static void UpdateTotalBuyPricePatch(float amount, MyStoreBlockViewModel __instance)
+        {
+            long num = 0L;
+            if (!float.IsNaN(amount))
+            {
+                long longAmount = (long)amount;
+                num = (longAmount * __instance.SelectedOfferItem.PricePerUnit);
+            }
+            __instance.TotalPriceToBuy = MyBankingSystem.GetFormatedValue(num, false);
+            __instance.IsBuyEnabled = (MyBankingSystem.GetBalance(MySession.Static.LocalCharacterEntityId) >= num && amount != 0f && !float.IsNaN(amount));
+            return;
+        }
+
+        private static void UpdateTotalSellPricePatch(float amount, MyStoreBlockViewModel __instance)
+        {
+            long num = 0L;
+            if (!float.IsNaN(amount))
+            {
+                long longAmount = (long)amount;
+                num = (longAmount * __instance.SelectedOrderItem.PricePerUnit);
+            }
+            __instance.TotalPriceToSell = MyBankingSystem.GetFormatedValue(num, false);
+            __instance.IsSellEnabled = ((float)__instance.SelectedOrderItem.Amount >= amount && amount != 0f && !float.IsNaN(amount));
+            return;
+        }
+    }
+}
